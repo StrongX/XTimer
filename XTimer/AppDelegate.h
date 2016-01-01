0937ab01e8bafc95f6c145ec9e9d742bde3e71e7
StrongX <385135499@qq.com>
2016-01-01 20:22:17 +0800

    Initial Commit

---
+++ XTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTimer
+//
+//  Created by xlx on 16/1/1.
+//  Copyright © 2016年 xlx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XTimer
+//
+//  Created by xlx on 16/1/1.
+//  Copyright © 2016年 xlx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
